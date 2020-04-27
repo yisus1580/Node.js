1e5b8eb11d2fc6cf959f8be829db69578875ff74
yisus1580 <tempor1580@outlook.com>
2020-04-26 23:06:44 -0500

    post agregado

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const express= require("express");
+const morgan = require('morgan');
+const express= require('express');
 const app = express();
+const pokemon = require('./routes/pokemon')
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res,next)=>{
-	res.send("Bienvenido");
+	return res.status(200).json({code: 1 , message:"Bienvenido al Pokedex"});
 });
 
-app.get("/:name",(req,res,next)=>{
-	console.log(req.params.name);
-	res.status(200);
-	res.send("Hola " + req.params.name);
+app.use("/pokemon", pokemon);
+
+app.use((req,res,next) => {
+	return res.status(404).json({code:404, message:"Url no encontrado" });
+} );
+
+app.listen(process.env.PORT || 3000,() =>{
+	console.log("Server is running better go catch it ");
 });
 
-app.listen(3000,() =>{console.log("Server is running better go catch it ");
-});
