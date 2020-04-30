aca56bdcf849fe71ccf35fa4265e21e3761caab2
yisus1580 <tempor1580@outlook.com>
2020-04-29 19:40:40 -0500

    patch,pu,delete

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -18,9 +18,57 @@ pokemon.post("/",async (req,res,next) => {
 	return res.status(201).json({code:201 , message:"pokemon aparecio milagrosamente de repente"});
 	}
 
-		return res.status (500).json({code:500, message:"Ocurrio un error"})
+		return res.status (500).json({code:500, message:"Ocurrio un error"});
 	}
-  return res.status (500).json({code:500, message:"Campos incompletos"})
+  return res.status (500).json({code:500, message:"Campos incompletos"});
+});
+
+pokemon.delete("/:id([0-9]{1,3})",async(req,res,next) =>{
+	const query = `DELETE FROM pokemon WHERE pok_id=${req.params.id}`;
+
+	const rows= await db.query(query);
+
+	if (rows.affectedRows==1) {
+		return res.status(200).json({code:200, message: "Pokemon borrado correctamente"})
+
+	}
+	return res.status(404).json({code:404, message: "Pokemon no encontrado" })
+});
+
+pokemon.put("/:id([0-9]{1,3})",async(req,res,next)=> {
+	const { pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+
+	if(pok_name&&pok_height&&pok_weight&&pok_base_experience){
+	let query = `UPDATE pokemon SET  pok_name='${pok_name}', pok_height=${pok_height},`;
+	query += `pok_weight=${pok_weight}, pok_base_experience=${pok_base_experience} WHERE pok_id=${req.params.id};`
+
+	const rows= await db.query(query);
+	
+
+	if(rows.affectedRows == 1){
+
+	return res.status(200).json({code:200 , message:"pokemon se adapto a nuevas circustancias"});
+	}
+
+	return res.status (500).json({code:500, message:"Ocurrio un error"});
+	}
+  return res.status (500).json({code:500, message:"Campos incompletos"});
+	
+});
+
+pokemon.patch("/:id([0-9]{1,3})",async(req,res,next)=> {
+	if (req.body.pok_name) {
+	let query = `UPDATE pokemon SET  pok_name='${req.body.pok_name}' WHERE pok_id=${req.params.id}`; 
+	const rows= await db.query(query);
+	
+
+	if(rows.affectedRows == 1){
+
+	return res.status(200).json({code:200 , message:"pokemon se adapto a nuevas circustancias"});
+	}
+	return res.status(500).jspon({code:500 , message: " Ocurrio un accidente deal with it "});
+} 
+	return res.status (500).json({code:500, message:"Campos incompletos"});
 });
 
 pokemon.get('/',async(req,res,next)=>{
