4b3b383d492cd05743893f40e87fa2250a95dd42
yisus1580 <tempor1580@outlook.com>
2020-05-18 00:04:20 -0500

    cors

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const User=require('./routes/User');
 const auth =require('./middleware/auth'); 
 const notfound =require('./middleware/notfound'); 
 const index =require('./middleware/index');
+const cors =require('./middleware/cors');
 
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
